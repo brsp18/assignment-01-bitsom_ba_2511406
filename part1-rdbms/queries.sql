956ab0f312c5010800541bf376da6901b87cd567
brsp18 <brswethapriya@gmail.com>
2026-03-23 03:41:15 +0000

    updated files

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -1,4 +1,5 @@
 -- Q1: List all customers from Mumbai along with their total order value
+
 Select c.customer_name,SUM(ol.quantity * p.unit_price) AS total_value from customers c
 join orders o
 on c.customer_id = o.customer_id
@@ -12,6 +13,7 @@ group by c.customer_name;
 
 
 -- Q2: Find the top 3 products by total quantity sold
+
 select top 3 p.product_name, sum(ol.quantity) as total_quantity from products p
 join order_list ol
 on p.product_id = ol.product_id
@@ -21,6 +23,7 @@ order by total_quantity DESC;
 
 
 -- Q3: List all sales representatives and the number of unique customers they have handled
+
 select COUNT(DISTINCT c.customer_id) as customer_count, o.sales_rep_id  
 from orders o
 join customers c
@@ -32,12 +35,13 @@ group by o.sales_rep_id;
 
 
 -- Q4: Find all orders where the total value exceeds 10,000, sorted by value descending
+
 Select o.order_id, SUM(ol.quantity * p.unit_price) as total_value
 from orders o
 join order_list ol
-    on o.order_id = ol.order_id
+on o.order_id = ol.order_id
 join products p
-    on ol.product_id = p.product_id
+on ol.product_id = p.product_id
 group by o.order_id
 having SUM(ol.quantity * p.unit_price) > 10000
 order by total_value desc;
@@ -47,8 +51,9 @@ order by total_value desc;
 
 
 -- Q5: Identify any products that have never been ordered
+
 select p.product_id, p.product_name
 from products p
 left join order_list ol
-    on p.product_id = ol.product_id
+on p.product_id = ol.product_id
 where ol.product_id Is null;
